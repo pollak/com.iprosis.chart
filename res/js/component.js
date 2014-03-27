a3b7fa10bc252ff38de02334793c28d25ccb98a7
pollak <amit.pollak@iprosis.com>
2014-03-27 08:14:32 +0200

    remodeling
    moving the code into resultsetToArray function

--- res/js/component.js
+++ res/js/component.js
@@ -2,30 +2,38 @@
 sap.designstudio.sdk.Component.subclass("com.iprosis.chart.chart", function() {
 
 	var that = this;
-	var dataResultSet = null;
+	var dataResultSet = null, xlenght = null, ylenght = null;
 	var Data = null
 		
 	this.init = function() {
+		// initialization Data (multi-level array) in dataResultSet size -{
+		xlenght = dataResultSet.axis_columns.length;
+		ylenght = dataResultSet.axis_rows.length;
+		this.Data = new Array(xlenght)
+		for (i=0 ; i < xlenght ; i++)
+			this.Data[i]=new Float64Array(ylenght)
+		// }-
+		
 		this.$().click(function() {
 			that.fireEvent("onclick");
 		});
 	};
 	
 	this.afterUpdate = function() {
-		var xlenght = dataResultSet.axis_columns.length;
-		var ylenght = dataResultSet.axis_rows.length;
-		this.Data = new Array(xlenght)
-		for (i=0 ; i < xlenght ; i++)
-			this.Data[i]=new Float64Array(ylenght)
+		this.resultsetToArray();
+	};
+	
+	this.resultsetToArray = function(){
+	// function for creating Data (multi-level array) contains DataResultSet 
 		var index = 0;
 		  for(var y=0 ; y < ylenght ; y++){
 			  for(var x=0 ; x < xlenght ; x++){
 			 	this.Data[x][y]=dataResultSet.data[index];
 			 	index++;
-			  }
-		  }
+			  };
+		  };
 		  
-		  // this.Data contains DataResultSet as an multi-level array
+		  
 	};
 	
 	this.OnClick = function(value) {
